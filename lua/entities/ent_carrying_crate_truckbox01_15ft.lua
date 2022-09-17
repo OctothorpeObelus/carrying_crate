543118a56926eb21d50bef680b2287688d9cdb73
Geemar <epicdj45@gmail.com>
2022-09-17 17:37:27 -0400

    Added 10ft and 15ft box truck boxes
    Should probably make proper textures for the 10 foot but I am lazy and retarded

---
+++ lua/entities/ent_carrying_crate_truckbox01_15ft.lua
@@ -0,0 +1,59 @@
+AddCSLuaFile()
+
+ENT.Type        = "anim"
+ENT.PrintName   = "15ft Truck Box"
+ENT.Author      = "Geemar"
+
+ENT.Category = "Carrying Crates"
+ENT.Spawnable       = true
+ENT.AdminSpawnable  = false
+
+--Crate attributes
+ENT.Model = "models/carriers/truckbox_15ft.mdl"
+ENT.AccumulateMass = true
+
+-- Whitelist of entity classes that can be put in the box.
+ENT.Whitelist = {
+    "prop_physics",
+    "prop_vehicle_prisoner_pod"
+}
+ENT.Box = {
+    max = Vector(-87.5, 42, 1.4),
+    min = Vector(87.5, -42, 82)
+}
+
+ENT.Sounds = {          --Optional parameter
+    on = "carrying_crates/cargo_common_pack.wav",
+    off = "carrying_crates/cargo_common_unpack.wav",
+    tipsound = "carrying_crates/cargo_common_unpack.wav"
+}
+
+if SERVER then
+    function ENT:Initialize()
+        carrying_crate.init(self)
+    end
+
+    function ENT:Use(activator, caller, useType, value)
+        if activator:KeyPressed(IN_USE) then
+            self.lastuser = activator
+            carrying_crate.used(self)
+            if self.hasLoad then
+                self:SetBodygroup(1,0)
+            else
+                self:SetBodygroup(1,1)
+            end
+        end
+    end
+
+    function ENT:Think()
+        carrying_crate.think(self)
+    end
+    
+    function ENT:OnRemove()
+        carrying_crate.onremove(self)
+    end
+else --Clientside
+    function ENT:Draw()
+        self:DrawModel()
+    end
+end
